ed572fc776807fb4e52f35545063c5f74e57c266
Chuck Clift <chuckcclift@gmail.com>
2017-11-25 14:24:29 -0500

    moved to python .ini configparser file to simplify config file parsing and allow for 4 fields per site

--- page2feed.py
+++ page2feed.py
@@ -3,24 +3,34 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
+import urllib
+import argparse
+import configparser
 
 
-for a in sys.stdin:
-    line = a.strip().split()
-    url = line[0]
-    source = line[1]
 
-    end_link_selector = line.index("a") + 1
-    link_selector = " ".join(line[2:end_link_selector])
-    title_selector = " ".join(line[end_link_selector:])
-    
-    doc = BeautifulSoup(requests.get(url).text, "html.parser")
-    print(source)
+parser = argparse.ArgumentParser(description="fetch feed from all urls specified in config file")
+parser.add_argument("config", help="input config file")
+args = parser.parse_args()
 
-    links = [link for link in doc.select(link_selector)]
-    titles = [t for t in doc.select(title_selector)]
-    
-    for l, t in zip(links, titles):
-        print("title: ", t)
-        print("  link: ", l.get("href"))
+config = configparser.RawConfigParser()
+config.read(args.config)
 
+for s in config.sections():
+    url = config.get(s, "url")
+    section_selector = config.get(s, "section")
+    link_selector = config.get(s, "link")
+    title_selector = config.get(s, "title")
+
+    scheme = urlparse(url).scheme
+    host = urlparse(url).netloc
+     
+    doc = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
+
+    for section in doc.select(section_selector):
+        t = section.select(title_selector)[0]
+        l = section.select(link_selector)
+        for link in l:
+            print(link.get("href"), t.get_text())
+            
